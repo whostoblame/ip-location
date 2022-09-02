30fc5e94b43f2a32de2900eedfa1340d44fcc5fd
Benedikt Kaltenecker <benedikt@kaltenecker.org>
2022-09-02 16:37:46 +0200

    Axios Request and IP Display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import axios from 'axios';
+import react, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [ip, setIp] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("https://geo.ipify.org/api/v2/country?apiKey=at_Tlen5IDNZUSsrlwOicHgbPmr1TJuo")
+      .then((response) => {
+        console.log(response)
+        console.log(response.data)
+        setIp(response.data.ip)
+      }).catch((error) => console.log(error)
+      )
+  }, []);
+
+
+
   return (
     <div className="App">
-      
+      <div className="ip-field">
+        <p>Your IP is: {ip}</p>
+      </div>
+      <div className="map-field"></div>
+      <div className="ip-infos"></div>
+
     </div>
   );
 }
